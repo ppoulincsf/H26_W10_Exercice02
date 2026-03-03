07de11b9b2ae953d3e86ffc8581c4a5b5792711a
ppoulincsf <ppoulin@csfoy.ca>
2026-03-03 17:00:32 -0500

    Initial commit

---
+++ Exercice02/Program.cs
@@ -0,0 +1,152 @@
+﻿namespace Exercice02
+{
+  public class Program
+  {
+    public const float REBATE_RATE = 0.10f;
+    public const float TPS_RATE = 0.05f;
+    public const float TVQ_RATE = 0.09975f;
+
+    public static void Main ( string[] args )
+    {
+      SquarePerimeter ();          // Exécution de la solution du problème #1
+      NumbersAverage ();           // Exécution de la solution du problème #2
+      ApplyRebate ();              // Exécution de la solution du problème #3
+      SwapNumbers ();              // Exécution de la solution du problème #4
+      ComputePriceWithTaxes ();    // Exécution de la solution du problème #5
+    }
+
+
+
+
+
+
+    public static void SquarePerimeter ()
+    {
+      // Entrées
+
+
+      // Algorithme 
+
+
+      // Sortie
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void NumbersAverage ()
+    {
+      // Entrées
+
+
+      // Algorithme 
+
+
+      // Sortie
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void ApplyRebate ()
+    {
+      // Entrées
+
+
+      // Algorithme 
+
+
+      // Sortie
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void SwapNumbers ()
+    {
+      // Entrées
+
+
+      // Algorithme 
+
+
+      // Sortie
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void ComputePriceWithTaxes ()
+    {
+      // Entrées
+
+
+      // Algorithme 
+
+
+      // Sortie
+
+    }
+  }
+}
